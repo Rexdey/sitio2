fc9d5f1668aeb5f71da02e038de37e9d7b1a2ee3
Rexdey <rexdey@gmail.com>
2017-03-24 03:26:47 -0300

    carga funcionando

--- phpLogin/cargarImagen.php
+++ phpLogin/cargarImagen.php
@@ -2,24 +2,80 @@
 require_once('dbConnect.php');
 	$msg = "";
 	if (isset($_POST['upload'])) {
-		$target = "images/".basename($_FILES['image']['name']);
 
-		$image = $_FILES['image']['name'];
-		$text = $_POST['text'];
-		$sql = "INSERT INTO certificados (ruta, sello) VALUES ('$image', '$text')";
-		if ($con->query($sql) === TRUE) {
+    $text = $_POST['text'];
+    if (($_FILES["image"]["type"] == "image/jpeg")
+     || ($_FILES["image"]["type"] == "image/jpg"))
+     {
 
-		if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-			$msg = "Image Uploaded Successfully";
+		  $target = "images/". $text .".jpg";
+		  $image = $_FILES['image']['name'];
 
-		} else {
-			$msg = "There Was A problem uploading image";
-		}
-  }else{
-    echo "Error de base de datos." . $sql . "<br>" . $con->error;
+		  $sql = "INSERT INTO certificados (ruta, sello) VALUES ('$image', '$text')";
+		  if ($con->query($sql) === TRUE) {
+
+		    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+			   $msg = "Image Uploaded Successfully";
+
+		    } else {
+			   $msg = "There Was A problem uploading image";
+		  }
+      }else{
+      echo "Error de base de datos." . $sql . "<br>" . $con->error;
+    }
+    //
+  }elseif ($_FILES["image"]["type"] == "application/pdf") {
+
+    $pdfAbsolutePath = __DIR__."/images/test.pdf";
+
+    if (move_uploaded_file($_FILES['image']["tmp_name"], $pdfAbsolutePath)) {
+
+          $im    = new imagick($pdfAbsolutePath);
+
+          $noOfPagesInPDF = $im->getNumberImages();
+
+          if ($noOfPagesInPDF) {
+
+              for ($i = 0; $i < $noOfPagesInPDF; $i++) {
+
+                  $url = $pdfAbsolutePath.'['.$i.']';
+
+                  $image = new Imagick();
+                  $image->setResolution(150,150);
+                  //$image->setSize(1584,1224);
+                  $image->setSize(800,600);
+                  $image->readimage($url);
+                  $image->setImageFormat("jpg");
+                  //$image->setImageCompression(imagick::COMPRESSION_JPEG);
+                  //$image->setImageCompressionQuality(100);
+
+                  $image->writeImage(__DIR__."/images/".$text.'.jpg');
+
+              }
+
+              echo "All pages of PDF is converted to images";
+
+          }
+          echo "PDF doesn't have any pages";
+
+    }
+
+  }else {
+    echo "el archivo no es un formato permitido";
   }
+  ////
 	}
 ?>
+///
+
+<?php
+
+
+
+ ?>
+
+
+///
 <!DOCTYPE html>
 <html>
 	<head>
@@ -28,21 +84,6 @@ require_once('dbConnect.php');
 	</head>
 	<body>
 		<div id="content">
-
-		<!-- <?php
-
-			$sql = "SELECT * FROM certificados";
-			$result = $con->query($sql);
-			while($row = $result->fetch_assoc()) {
-				echo "<div id='img_div'>";
-					echo "<img src='images/". $row['ruta']."'>";
-					echo "<p>" . $row['sello']."</p>";
-				echo "</div>";
-			}
-
-
-		?> -->
-
 
 			<form method="post" action="cargarimagen.php" enctype="multipart/form-data">
 				<input type="hidden" name="size" value="1000000" />
