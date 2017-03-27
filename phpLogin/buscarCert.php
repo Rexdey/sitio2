dcd7448fd93c54db6ff658f3c794dfa3ba07c50d
Rexdey <rexdey@gmail.com>
2017-03-27 01:58:37 -0300

    validacion buscarcert

--- phpLogin/buscarCert.php
+++ phpLogin/buscarCert.php
@@ -4,53 +4,7 @@ include ('validacionUser.php');
 ?>
 
 
-<?php
 
-require_once('dbConnect.php');
-$selloErr="";
-$sello="";
-
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-
-    if (empty($_POST["sello"])) {
-      $selloErr = "Se necesita un numero de sello";
-
-    } else {
-      $sello = test_input($_POST["sello"]);
-
-      if (!preg_match("/^[0-9]*$/",$sello)) {
-        $selloErr= "Solo se permiten numeros sin espacios en blanco";
-
-      }
-      else{
-
-        $sql = "SELECT * FROM certificados WHERE sello=$sello";
-        $result = $con->query($sql);
-
-
-        if($result->num_rows < 1){
-          $selloErr ="El codigo ingresado no existe";
-        }else{
-
-        while($row = $result->fetch_assoc()) {
-          $sello=  $row["sello"];
-          $id_certificado= $row["id_certificado"];
-          echo "<div id='img_div'>";
-          echo "<img src='images/". $row['ruta']."'>";
-          echo "</div>";
-          $_SESSION['sello'] = $sello;
-          }
-      }
-    }
-
-
-  }
-
-  }
- mysqli_close($con);
-?>
 
 
 
@@ -92,11 +46,66 @@ $sello="";
  <h1>Buscar Certificados</h1>
    <hr />
 
-   <p><span class="error">* campo obligatorio.</span></p>
+   <div>
+     <?php
+
+     require_once('dbConnect.php');
+     $selloErr="";
+     $sello="";
+     $down="";
+     $descErr="Necesita buscar un certificado para descargar";
+
+       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+         if (empty($_POST["sello"])) {
+           $selloErr = "Se necesita un numero de sello";
+
+         } else {
+           $sello = test_input($_POST["sello"]);
+
+           if (!preg_match("/^[0-9]*$/",$sello)) {
+             $selloErr= "Solo se permiten numeros sin espacios en blanco";
+
+           }
+           else{
+
+             $sql = "SELECT * FROM certificados WHERE sello=$sello";
+             $result = $con->query($sql);
+
+
+             if($result->num_rows < 1){
+               $selloErr ="El codigo ingresado no existe";
+             }else{
+
+             while($row = $result->fetch_assoc()) {
+               $sello=  $row["sello"];
+               $id_certificado= $row["id_certificado"];
+               echo "<div id='img_div'>";
+               echo "<img src='images/". $row['ruta']."'>";
+               echo "</div>";
+               $_SESSION['sello'] = $sello;
+               $down="des1.php";
+               $descErr="";
+               }
+           }
+         }
+
+
+       }
+
+       }
+      mysqli_close($con);
+     ?>
+
+
+   </div>
+
+   <!-- <p><span class="error">* campo obligatorio.</span></p> -->
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
      <hr/>
-
+     <p>Ingrese el numero de sello del certificado a buscar (Campo obligatorio)</p>
      <label for="sell">Sello:</label><br>
      <input type="text" name="sello" maxlength="32" value="<?php echo $sello;?>" ><br>
      <span class="error">* <?php echo $selloErr;?></span>
@@ -115,15 +124,16 @@ $sello="";
      </form>
 
     <hr/><br/>
+<h1>Descargar el Certificado encontrado</h1>
+    <form method="post" action="<?php echo $down ?>">
 
-    <form method="post" action="des1.php">
-     <hr/>
 
-
+<p>Añada un numero de patente o chassis (Campo Opcional)</p>
 
      <label for="pat">Patente/Chassis:</label><br>
      <input type="text" name="patente" maxlength="32" ><br>
      <input type="hidden" name="sello2" value="<?php echo $sello;?>" />
+     <span class="error">* <?php echo $descErr;?></span>
      <br/><br/>
 
 
