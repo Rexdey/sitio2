b9c2dc87feb6bd36fd3a9f87653133de58fb5898
Rexdey <rexdey@gmail.com>
2017-03-27 04:16:16 -0300

    pulir buscador2

--- phpLogin/adminCert.php
+++ phpLogin/adminCert.php
@@ -45,14 +45,17 @@ include ('sanitizar.php');
   <?php
 
   include('dbConnect.php');
-  //$selloErr="";
+  $selloErr="";
   $sello="";
   $down="";
   //$descErr="Necesita buscar un certificado para descargar";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+      if (empty($_POST["sello"])) {
 
+
+      }else {
       $sello = test_input($_POST["sello"]);
 
         if (!preg_match("/^[0-9]*$/",$sello)) {
@@ -71,7 +74,8 @@ include ('sanitizar.php');
 
         ///////////////////
 
-
+?>
+<?php
 echo "<table border=2>";
 echo "<tr> <th>Número de Sello</th>  <th>Fecha de creación</th>
 <th>Última modificación</th><th>Imprimir</th><th>Editar</th><th>Borrar</th></tr>";
@@ -102,32 +106,36 @@ mysqli_close($con);
 
 
 
-
+}
     }
 
   ?>
-
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-   <hr/>
-   <p>Ingrese el numero de sello del certificado a buscar</p>
-   <label for="sell">Sello:</label><br>
-   <input type="text" name="sello" maxlength="32" value="<?php echo $sello;?>" ><br>
-
-   <br/><br/>
-
-
-
-    <br/><br/>
-
-
-    <br/><br/>
-    <input type="submit" name="submit" value="Buscar">
-    <input type="reset" name="clear" value="Limpiar">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+ <hr/>
+ <p>Ingrese el numero de sello del certificado a buscar</p>
+ <label for="sell">Sello:</label><br>
+ <input type="text" name="sello" maxlength="32" value="" ><br>
+<span class="error">* <?php echo $selloErr;?></span>
+ <br/><br/>
 
 
 
 
-   </form>
+
+
+
+  <input type="submit" name="submit" value="Buscar">
+  <input type="reset" name="clear" value="Limpiar">
+
+
+
+
+ </form>
+
+
+  <br/><br/>
+
+  <br/><br/>
 
 
 </div>
@@ -178,11 +186,11 @@ mysqli_close($con);
 	$prev = $startrow - 10;
 echo "<div align='left'>";
 	if ($prev >= 0)
-	    echo '<a href="'.$_SERVER['PHP_SELF'].'?startrow='.$prev.'"> Previous </a>';
+	    echo '<a href="'.$_SERVER['PHP_SELF'].'?startrow='.$prev.'"> Siguiente </a>';
 echo "</div>";
 	echo " ";
 echo "<div align='right'>";
-	echo '<a href="'.$_SERVER['PHP_SELF'].'?startrow='.($startrow+10).'"> Next </a>';
+	echo '<a href="'.$_SERVER['PHP_SELF'].'?startrow='.($startrow+10).'"> Anterior </a>';
 echo "</div>";
 	 ?>
 	 <div align="center">
