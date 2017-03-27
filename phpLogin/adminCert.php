13809a298cac7ae3e2f0678b1c16e3704c4b2544
Rexdey <rexdey@gmail.com>
2017-03-27 02:42:38 -0300

    buscar cert menu admin

--- phpLogin/adminCert.php
+++ phpLogin/adminCert.php
@@ -1,5 +1,6 @@
 <?php
 include ('validacionSesion.php');
+include ('sanitizar.php');
 
 ?>
 
@@ -36,11 +37,107 @@ include ('validacionSesion.php');
 <h1>Administrar Certificados</h1>
   <hr />
 
+
+
+
+<div>
+
+  <?php
+
+  include('dbConnect.php');
+  //$selloErr="";
+  $sello="";
+  $down="";
+  //$descErr="Necesita buscar un certificado para descargar";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+      $sello = test_input($_POST["sello"]);
+
+        if (!preg_match("/^[0-9]*$/",$sello)) {
+          $selloErr= "Solo se permiten numeros sin espacios en blanco";
+
+        }
+        else{
+
+          $sql = "SELECT * FROM certificados WHERE sello=$sello";
+          $result = $con->query($sql);
+
+
+          if($result->num_rows < 1){
+            $selloErr ="El codigo ingresado no existe";
+          }else{
+
+        ///////////////////
+
+
+echo "<table border=2>";
+echo "<tr> <th>Número de Sello</th>  <th>Fecha de creación</th>
+<th>Última modificación</th><th>Imprimir</th><th>Editar</th><th>Borrar</th></tr>";
+
+while($row = $result->fetch_assoc()) {
+
+    echo "<tr >";
+    echo "<td>" . $row["sello"]. "</td>";
+    echo "<td>" . $row["fecha"]. "</td>";
+    echo "<td>" . $row["ult_mod"]. "</td>";
+    echo '<td><a href="ImprimirCertAdmin.php?id=' . $row["id_certificado"] . '"> Imprimir </a></td>';
+    echo '<td><a href="verCert.php?id=' . $row["id_certificado"] . '"> Editar </a></td>';
+    echo '<td><a href="borrarCert.php?id=' . $row["id_certificado"] . '"> Borrar </a></td>';
+
+    echo "</tr>";
+}
+echo"</table>";
+
+mysqli_close($con);
+
+
+
+
+
+            /////////////
+        }
+      }
+
+
+
+
+    }
+
+  ?>
+
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+   <hr/>
+   <p>Ingrese el numero de sello del certificado a buscar</p>
+   <label for="sell">Sello:</label><br>
+   <input type="text" name="sello" maxlength="32" value="<?php echo $sello;?>" ><br>
+
+   <br/><br/>
+
+
+
+    <br/><br/>
+
+
+    <br/><br/>
+    <input type="submit" name="submit" value="Buscar">
+    <input type="reset" name="clear" value="Limpiar">
+
+
+
+
+   </form>
+
+
+</div>
+</div>
+
 	<form method='get'>
 
 	<?php
 
-	require_once('dbConnect.php');
+	include('dbConnect.php');
 
 	if (!isset($_GET['startrow']) or !is_numeric($_GET['startrow'])) {
 	//we give the value of the starting row to 0 because nothing was found in URL
@@ -92,7 +189,7 @@ echo "</div>";
 	 <a href='portal-admin.php'>Volver</a>
 
 	</form>
-
+</div>
   <hr />
 </div>
 </div>
